647f31e64e888dafe8ff871251aadce71f0ca53c
mohamad <mk.farhat@hotmail.com>
2020-09-29 22:51:58 +1000

    Registration Changes

--- onlineStoreTemplate/resources/views/Properties/index.blade.php
+++ onlineStoreTemplate/resources/views/Properties/index.blade.php
@@ -2,25 +2,25 @@
 
 @section('content')
 
-    <div class="container bg-light">
+    <div class="">
 
         @if(count($properties)>0)
             @if (Request::is('properties/rent'))
-                <h2>Apartments for rent:</h2>
+                <h2>Properties for rent</h2>
             @else
-                <h2>Apartments for sell:</h2>
+                <h2>Properties for sale</h2>
             @endif
 
             @foreach($properties as $property)
-                <div class="bg-dark container text-white">
-                    <div id="carouselExampleFade-{{$property->id}}" class="carousel slide carousel-fade col-md-6"
+                <div class="post" style="height: 400px;">
+                    <div id="carouselExampleFade-{{$property->id}}" class="carousel slide carousel-fade col-md-6 py-0 m-0"
                          data-ride="carousel">
                         <div class="carousel-inner">
                             @foreach($property->images as $image)
 
                                 <div class="carousel-item @if($loop->first) active @endif">
                                     <div style="width: 100%; height: 100%;">
-                                        <img style="height: 300px; width: 500px;"
+                                        <img style="height: 300px; "
                                              src="{{url('/storage/properties_images/' . $image->url)}}"
                                              alt="Second slide">
                                     </div>
@@ -30,18 +30,17 @@
                         </div>
                         <a class="carousel-control-prev" href="#carouselExampleFade-{{$property->id}}" role="button"
                            data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
+                            <i class="fa fa-chevron-left" aria-hidden="true"></i>
                         </a>
                         <a class="carousel-control-next" href="#carouselExampleFade-{{$property->id}}" role="button"
                            data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
+                            <i class="fa fa-chevron-right" aria-hidden="true"></i>
                         </a>
                     </div>
 
                     @if($property->showPrice == 1){{$property->price}} $ @else <h4>Contact the agent for price</h4> @endif
-                    <p><a href="/properties/{{$property->id}}">View details</a></p>
+
+                    <p><a  href="/properties/{{$property->id}}">View details</a></p>
                 </div>
                 <div class="p-1"></div>
             @endforeach
--- onlineStoreTemplate/resources/views/evaluate.blade.php
+++ onlineStoreTemplate/resources/views/evaluate.blade.php
@@ -5,4 +5,125 @@
 
     <h1>EVALUATE</h1>
 
+
+    <div >
+        <div class="card-header ">
+
+            <h5 class="card-title text-center">Evaluation Form</h5>
+        </div>
+        <form method="POST" action="" enctype="multipart/form-data">
+            @csrf
+
+            <input id="role" type="hidden" name="role" value=2>
+
+
+            <div class="form-label-group">
+
+                <label for="name">{{ __('Name') }}*</label>
+
+
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                       name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                @error('name')
+                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                @enderror
+
+
+            </div>
+
+
+            <div class="form-label-group">
+                <label for="email">{{ __('E-Mail Address') }}*</label>
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                       name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                @enderror
+            </div>
+
+            <div class="form-label-group">
+                <label for="phoneNumber">Phone Number*</label>
+                <select id="phoneNumberCode" name="phoneNumberCode" class="form-control">
+                    <option value="+61">+61</option>
+                </select>
+                <input id="phoneNumber" type="number"
+                       class="form-control @error('Phone Number') is-invalid @enderror" name="phoneNumber"
+                       value="{{ old('phoneNumber') }}" required autocomplete="phoneNumber" autofocus>
+                @error('phoneNumber')
+                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                @enderror
+            </div>
+
+
+            <div class="form-label-group">
+
+                <label for="location">Location</label>
+
+
+                <input id="location" type="text" class="form-control @error('location') is-invalid @enderror"
+                       name="location" value="{{ old('location') }}" required autocomplete="location" autofocus>
+                @error('location')
+                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                @enderror
+
+
+            </div>
+            <div class="form-label-group">
+
+                <label for="description">Description</label>
+
+
+                <input id="description" type="text" class="form-control @error('description') is-invalid @enderror"
+                       name="description" value="{{ old('description') }}" required autocomplete="description" autofocus>
+                @error('description')
+                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                @enderror
+
+
+            </div>
+
+
+
+
+            <div class="form-label-group">
+                <label for="num_bed">Number of bedrooms</label>
+
+                <input id="num_bed" type="number"
+                       class="form-control @error('num_bed') is-invalid @enderror" name="num_bed"
+                       value="{{ old('num_bed') }}" required autocomplete="num_bed" autofocus>
+                @error('num_bed')
+                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                @enderror
+            </div>
+
+
+
+
+
+
+
+
+            <div class="form-group row mb-0">
+                <div class="col-md-6 ">
+                    <button type="submit" class="btn btn-primary1">
+                        Send
+                    </button>
+                </div>
+            </div>
+        </form>
+
+    </div>
 @endsection
