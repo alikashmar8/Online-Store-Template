06857fc1a9cb230137440186ede0eabb32f1b72a
reda <mk.farhat@hotmail.com>
2020-10-24 15:15:25 +1100

    link google locations to the search bar

--- onlineStoreTemplate/resources/views/Properties/index.blade.php
+++ onlineStoreTemplate/resources/views/Properties/index.blade.php
@@ -91,8 +91,10 @@
                                             </div>
 
                                             <div class="search-bar-section">
-                                                <input class="location1" type="search" name="location"
-                                                       hint="Search by location" placeholder="Search by location">
+                                                {{--<input class="location1" type="search" name="location"
+                                                        hint="Search by location" placeholder="Search by location">--}}
+                                                <input class="location1" type="search" placeholder="Filter by Location" {{--name="address"--}} name="location"
+                                                       onFocus="initializeAutocomplete()" id="locality" >
 
 
                                                 <button type="submit" Class="submit btn-primary1">Search
@@ -352,5 +354,42 @@
         });
     </script>
 
+
+
+    {{--scripts for google locations--}}
+    <script
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB1CbPQ2HCLV38r9m68B8VCv51JBVke5TM&callback=initAutocomplete&libraries=places&v=weekly"
+        defer
+    ></script>
+    <script type="text/javascript">
+        function initializeAutocomplete() {
+            var input = document.getElementById('locality');
+            // var options = {
+            //   types: ['(regions)'],
+            //   componentRestrictions: {country: "IN"}
+            // };
+            var options = {}
+
+            var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+            google.maps.event.addListener(autocomplete, 'place_changed', function() {
+                var place = autocomplete.getPlace();
+                var lat = place.geometry.location.lat();
+                var lng = place.geometry.location.lng();
+                var placeId = place.place_id;
+                // to set city name, using the locality param
+                var componentForm = {
+                    locality: 'short_name',
+                };
+                for (var i = 0; i < place.address_components.length; i++) {
+                    var addressType = place.address_components[i].types[0];
+                    if (componentForm[addressType]) {
+                        var val = place.address_components[i][componentForm[addressType]];
+                        document.getElementById("city").value = val;
+                    }
+                }
+            });
+        }
+    </script>
 
 @endsection
--- onlineStoreTemplate/resources/views/welcome.blade.php
+++ onlineStoreTemplate/resources/views/welcome.blade.php
@@ -54,12 +54,17 @@
                         </div>
 
                         <div class="search-bar-section">
-                            <input class="location1" type="search" name="location"
-                                   hint="Search by location" placeholder="Search by location">
+
+                            <input class="location1" type="search" placeholder="Search by Location" {{--name="address"--}} name="location"
+                                   onFocus="initializeAutocomplete()" id="locality" >
 
 
                             <button type="submit"   class="btn-primary1 submit">Search
                             </button>
+
+
+
+
                         </div>
 
                         <div class="search-bar-section">
@@ -245,4 +250,39 @@
         }
     </script>
 
+    {{--scripts for google locations--}}
+<script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB1CbPQ2HCLV38r9m68B8VCv51JBVke5TM&callback=initAutocomplete&libraries=places&v=weekly"
+    defer
+></script>
+<script type="text/javascript">
+    function initializeAutocomplete() {
+        var input = document.getElementById('locality');
+        // var options = {
+        //   types: ['(regions)'],
+        //   componentRestrictions: {country: "IN"}
+        // };
+        var options = {}
+
+        var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            var place = autocomplete.getPlace();
+            var lat = place.geometry.location.lat();
+            var lng = place.geometry.location.lng();
+            var placeId = place.place_id;
+            // to set city name, using the locality param
+            var componentForm = {
+                locality: 'short_name',
+            };
+            for (var i = 0; i < place.address_components.length; i++) {
+                var addressType = place.address_components[i].types[0];
+                if (componentForm[addressType]) {
+                    var val = place.address_components[i][componentForm[addressType]];
+                    document.getElementById("city").value = val;
+                }
+            }
+        });
+    }
+</script>
 @endsection
